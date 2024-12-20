2613f650858dc65b01a2ec52c9bca9e792632480
khan-shimul <khanshimul196@gmail.com>
2024-12-21 01:42:41 +0600

    single-item-get

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ async function run() {
       //   admin = user.role === 'admin'
       // }
       // res.send({admin});
-      const admin = user.role === 'admin';
+      const admin = user?.role === 'admin';
       res.send({admin})
     })
 
@@ -130,9 +130,37 @@ async function run() {
         const result = await menuCollection.find().toArray();
         res.send(result);
     });
+    app.get('/menu/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: id};
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
     app.post('/menu', verifyToken, verifyAdmin, async(req, res) => {
       const menuItem = req.body;
       const result = await menuCollection.insertOne(menuItem);
+      res.send(result);
+    });
+    app.patch('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const item = req.body;
+      const filter = {_id: id};
+      const updatedItem = {
+        $set: {
+          name: item.name,
+          image: item.image,
+          price: item.price,
+          category: item.category,
+          recipe: item.recipe
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updatedItem);
+      res.send(result)
+    })
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await menuCollection.deleteOne(query);
       res.send(result);
     })
     app.get('/reviews', async(req, res) => {
